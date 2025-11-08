578b213ed9823b5f7ef5e23f2b71886d1e7f1b1f
MelichPhotography <luke.adam.333@gmail.com>
2025-11-08 10:33:16 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Load config and build form
+fetch('config.json')
+  .then(res => res.json())
+  .then(config => {
+    document.getElementById('title').innerText = config.title;
+    const form = document.getElementById('qrForm');
+
+    config.fields.forEach(field => {
+      const label = document.createElement('label');
+      label.innerText = field.label;
+      const input = document.createElement('input');
+      input.type = field.type;
+      input.name = field.code;
+      if (field.required) input.required = true;
+      label.appendChild(input);
+      form.insertBefore(label, form.querySelector('button'));
+    });
+
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      const formData = new FormData(form);
+      const values = [];
+      config.fields.forEach(f => values.push(formData.get(f.code)));
+      const qrText = values.join(',');
+      document.getElementById('qr').innerHTML = '';
+      new QRCode(document.getElementById('qr'), qrText);
+    });
+  });
